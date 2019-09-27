4ddd545adca025be2d398f8384de471a99bcb041
nsane4stargate <lea.middleton6347@gmail.com>
2019-09-26 20:59:11 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyButtonComponent } from './my-button/my-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyImageComponent,
+    MyButtonComponent
   ],
   imports: [
     BrowserModule
